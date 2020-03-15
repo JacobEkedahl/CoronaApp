3a4a2adb0c4d55943bac6c42c85b7d87b2c3771c
Jacob Ekedahl <jacob.ekedahl@if.no>
2020-03-15 01:04:34 +0100

    Added a pub/sub function for fetching corona data and adding to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,311 @@
-const functions = require('firebase-functions');
+/* eslint-disable promise/always-return */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const siteUrl = "https://www.worldometers.info/coronavirus/#countries";
+const webhookUrl =
+  "https://discordapp.com/api/webhooks/688444307598082072/5xRtv3WSub2jE7np1RMdNh18xLBUnRHHAJce1fqLLqJSy51dA5vriAmLYEkbSXDSc6wf";
+const errorHook =
+  "https://discordapp.com/api/webhooks/688523920500064329/YtmNszuWwKwGREYKmQZfPVebJaeGbPs_6AiNrC-0n1nS3JenqoNy2g2KxSg8rWNog5ew";
+const axios = require("axios");
+const cheerio = require("cheerio");
+const cheerioTableparser = require("cheerio-tableparser");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+const db = admin.firestore();
+
+exports.scrapeAndUpdateData = async (pubSubEvent, context) => {
+  const tableData = await fetchData();
+  const countriesData = constructCountryInfo(tableData);
+  const timeStamp = admin.firestore.FieldValue.serverTimestamp();
+
+  countriesData.forEach(info => {
+    var docRef = db.collection("latestValues").doc(info["country"]);
+    var docRefHistory = db.collection("history").doc();
+
+    docRef
+      .get()
+      .then(doc => {
+        if (
+          !doc.exists ||
+          doc.data.cases !== info["cases"] ||
+          doc.data.critical !== info["critical"] ||
+          doc.data.recovered !== info["recovered"] ||
+          doc.data.deaths !== info["deaths"]
+        ) {
+          docRefHistory.set({
+            country: info["country"],
+            cases: info["cases"],
+            critical: info["critical"],
+            recovered: info["recovered"],
+            deaths: info["deaths"],
+            time: timeStamp
+          });
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        axios.post(errorHook, {
+          username: "HistoryUpdateFailure",
+          content: `Could not update history value for country ${info["country"]}, error: ${error}`
+        });
+      });
+
+    docRef.set(
+      {
+        cases: info["cases"],
+        critical: info["critical"],
+        recovered: info["recovered"],
+        deaths: info["deaths"]
+      },
+      { merge: true }
+    );
+  });
+};
+
+const constructCountryInfo = tableData => {
+  const totalCountries = [];
+  const totalCases = [];
+  const totalDeath = [];
+  const totalSerious = [];
+  const totalRecoveries = [];
+  const excludedCountries = [];
+  const excludeCountriesNames = [];
+
+  tableData.forEach(column => {
+    const title = column[0];
+
+    switch (title) {
+      case "Country,Other":
+        for (index = 1; index < column.length; index++) {
+          const country = column[index];
+          if (country in countries) {
+            totalCountries.push(country);
+          } else {
+            excludedCountries.push(index);
+            excludeCountriesNames.push(country);
+          }
+        }
+        break;
+
+      case "TotalCases":
+        for (index = 1; index < column.length; index++) {
+          const cases = column[index];
+          if (!(index in excludedCountries)) {
+            totalCases.push(cases);
+          }
+        }
+        break;
+
+      case "TotalDeaths":
+        for (index = 1; index < column.length; index++) {
+          const deaths = column[index];
+          if (!(index in excludedCountries)) {
+            totalDeath.push(deaths);
+          }
+        }
+        break;
+
+      case "TotalRecovered":
+        for (index = 1; index < column.length; index++) {
+          const recovered = column[index];
+          if (!(index in excludedCountries)) {
+            totalRecoveries.push(recovered);
+          }
+        }
+
+        break;
+      case "Serious,Critical":
+        for (index = 1; index < column.length; index++) {
+          const serious = column[index];
+          if (!(index in excludedCountries)) {
+            totalSerious.push(serious);
+          }
+        }
+    }
+  });
+
+  const result = [];
+  for (index = 0; index < totalCountries.length; index++) {
+    result.push({
+      country: totalCountries[index],
+      cases: totalCases[index],
+      critical: totalSerious[index],
+      recovered: totalRecoveries[index],
+      deaths: totalDeath[index]
+    });
+  }
+
+  if (excludeCountriesNames.length !== 0) {
+    axios.post(webhookUrl, {
+      username: "New countries with corona",
+      content: `These countries have not been added for translation: ${excludeCountriesNames.toString()}`
+    });
+  }
+
+  return result;
+};
+
+const fetchData = async () => {
+  const result = await axios.get(siteUrl);
+  const $ = cheerio.load(result.data);
+  cheerioTableparser($);
+  return $("#main_table_countries").parsetable(true, true, true);
+};
+
+const countries = {
+  Seychelles: "Seychelles",
+  Mayotte: "Mayotte",
+  "Total:": "Total",
+  China: "China",
+  Italy: "Italy",
+  Iran: "Iran",
+  "S. Korea": "S. Korea",
+  Spain: "Spain",
+  Germany: "Germany",
+  France: "France",
+  USA: "USA",
+  Switzerland: "Switzerland",
+  UK: "United Kingdom",
+  Norway: "Norway",
+  Netherlands: "Netherlands",
+  Sweden: "Sweden",
+  Denmark: "Denmark",
+  Japan: "Japan",
+  "Diamond Princess": "Diamond Princess",
+  Belgium: "Belgium",
+  Austria: "Austria",
+  Qatar: "Qatar",
+  Australia: "Australia",
+  Malaysia: "Malaysia",
+  Finland: "Finland",
+  Singapore: "Singapore",
+  Bahrain: "Bahrain",
+  Canada: "Canada",
+  Greece: "Greece",
+  Slovenia: "Slovenia",
+  Portugal: "Portugal",
+  Israel: "Israel",
+  Iceland: "Iceland",
+  Brazil: "Brazil",
+  Czechia: "Czechia",
+  "Hong Kong": "Hong Kong",
+  Philippines: "Philippines",
+  Estonia: "Estonia",
+  Kuwait: "Kuwait",
+  Romania: "Romania",
+  Iraq: "Iraq",
+  "San Marino": "San Marino",
+  Indonesia: "Indonesia",
+  Lebanon: "Lebanon",
+  Egypt: "Egypt",
+  Poland: "Poland",
+  Ireland: "Ireland",
+  "Saudi Arabia": "Saudi Arabia",
+  UAE: "UAE",
+  India: "India",
+  Thailand: "Thailand",
+  Russia: "Russia",
+  Taiwan: "Taiwan",
+  Vietnam: "Vietnam",
+  Luxembourg: "Luxembourg",
+  Chile: "Chile",
+  Serbia: "Serbia",
+  Albania: "Albania",
+  Peru: "Peru",
+  "South Africa": "South Africa",
+  Palestine: "Palestine",
+  Algeria: "Algeria",
+  Croatia: "Croatia",
+  Brunei: "Brunei",
+  Panama: "Panama",
+  Argentina: "Argentina",
+  Slovakia: "Slovakia",
+  Bulgaria: "Bulgaria",
+  Pakistan: "Pakistan",
+  Georgia: "Georgia",
+  Belarus: "Belarus",
+  Latvia: "Latvia",
+  Mexico: "Mexico",
+  "Costa Rica": "Costa Rica",
+  Hungary: "Hungary",
+  Ecuador: "Ecuador",
+  Colombia: "Colombia",
+  Senegal: "Senegal",
+  Cyprus: "Cyprus",
+  Oman: "Oman",
+  "Bosnia and Herzegovina": "Bosnia and Herzegovina",
+  Malta: "Malta",
+  Morocco: "Morocco",
+  Tunisia: "Tunisia",
+  Azerbaijan: "Azerbaijan",
+  Armenia: "Armenia",
+  "North Macedonia": "North Macedonia",
+  "Dominican Republic": "Dominican Republic",
+  Afghanistan: "Afghanistan",
+  Macao: "Macao",
+  "Sri Lanka": "Sri Lanka",
+  Bolivia: "Bolivia",
+  Maldives: "Maldives",
+  "Faeroe Islands": "Faeroe Islands",
+  Lithuania: "Lithuania",
+  Jamaica: "Jamaica",
+  Cambodia: "Cambodia",
+  Paraguay: "Paraguay",
+  "New Zealand": "New Zealand",
+  "French Guiana": "French Guiana",
+  Kazakhstan: "Kazakhstan",
+  Martinique: "Martinique",
+  Moldova: "Moldova",
+  Réunion: "Réunion",
+  Turkey: "Turkey",
+  Cuba: "Cuba",
+  Liechtenstein: "Liechtenstein",
+  Uruguay: "Uruguay",
+  Ukraine: "Ukraine",
+  Bangladesh: "Bangladesh",
+  "Channel Islands": "Channel Islands",
+  "French Polynesia": "French Polynesia",
+  "Puerto Rico": "Puerto Rico",
+  Monaco: "Monaco",
+  Nigeria: "Nigeria",
+  Aruba: "Aruba",
+  "Burkina Faso": "Burkina Faso",
+  Cameroon: "Cameroon",
+  DRC: "DRC",
+  Ghana: "Ghana",
+  Honduras: "Honduras",
+  Namibia: "Namibia",
+  "Saint Martin": "Saint Martin",
+  "Trinidad and Tobago": "Trinidad and Tobago",
+  Venezuela: "Venezuela",
+  Guyana: "Guyana",
+  Sudan: "Sudan",
+  Andorra: "Andorra",
+  Jordan: "Jordan",
+  Nepal: "Nepal",
+  "Antigua and Barbuda": "Antigua and Barbuda",
+  Bhutan: "Bhutan",
+  "Cayman Islands": "Cayman Islands",
+  "Ivory Coast": "Ivory Coast",
+  Curaçao: "Curaçao",
+  Ethiopia: "Ethiopia",
+  Gabon: "Gabon",
+  Gibraltar: "Gibraltar",
+  Guadeloupe: "Guadeloupe",
+  Guatemala: "Guatemala",
+  Guinea: "Guinea",
+  "Vatican City": "Vatican City",
+  Kenya: "Kenya",
+  Mauritania: "Mauritania",
+  Mongolia: "Mongolia",
+  Rwanda: "Rwanda",
+  "St. Barth": "St. Barth",
+  "Saint Lucia": "Saint Lucia",
+  "St. Vincent Grenadines": "St. Vincent Grenadines",
+  Suriname: "Suriname",
+  Eswatini: "Eswatini",
+  Togo: "Togo",
+  "U.S. Virgin Islands": "U.S. Virgin Islands"
+};
